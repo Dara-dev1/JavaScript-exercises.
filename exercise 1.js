854208baa33722a31732fc9b6b42ac03eaebe0ef
DaraTheDev <davidolukayode310@gmail.com>
2026-01-02 23:00:47 +0100

    Create exercise 1.js

---
+++ exercise 1.js
@@ -0,0 +1,58 @@
+//Exercise 1:
+
+//Question 1
+let firstName="John";
+let lastName="Doe";
+let age=25;
+//Solution:
+let intro="My name is"+" "+firstName+" "+lastName+" "+"and I am"+" "+age+" "+"years old."
+console.log(intro)
+//Ans:"My name is John Doe and I am 25 year old."
+
+//Question 2
+let num1=15;
+let num2=8;
+//Solution:
+let a=num1+num2;
+let b=num1-num2;
+let c=num1*num2;
+let d=num1/num2;
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+//Ans:23,7,120,1.875.
+
+//Question 3
+let temperature=22;
+//Solution:
+ if (temperature<20){
+   console.log("It's cold." )
+ }
+ else{
+   console.log("It's Warm.")
+ }
+ //Ans:It's Warm.
+ 
+//Question 4
+let price=50;
+let discount=10;
+//Solution:
+let finalPrice=price-discount;
+console.log(finalPrice)
+//Ans:40
+
+//Question 5
+let number=5
+//Solution:
+if(number>0){
+   console.log("Positive.")
+}
+else if (number<0){
+   console.log("Negative.")
+}
+else{
+   console.log("Zero.")
+}
+
+//Exercise 1 Done.
