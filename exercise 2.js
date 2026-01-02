c6c79c86a4472446d57da924531bf6d41fbbcfd0
DaraTheDev <davidolukayode310@gmail.com>
2026-01-02 23:03:43 +0100

    Add Exercise 2 with multiple questions and solutions

---
+++ exercise 2.js
@@ -0,0 +1,61 @@
+//Exercise 2
+
+//Question 6
+let score=75;
+//Solution:
+if (score>=50){
+   console.log("Pass.")
+}
+else{
+   console.log("Fail.")
+}
+//Ans:Pass.
+
+//Question 7
+let a=12;
+let b=15;
+//Solution
+if (a>b) {
+   console.log("12 is greater than 15.") 
+}
+else{
+   console.log("12 is not greater than 15.")
+}
+//Ans:12 is not greater than 15.
+
+//Question 8 
+let age=18;
+//Solution
+if (age<=18) {
+   console.log("Cannot vote.")
+}
+else if(age=18){
+   console.log("Can vote")
+}
+else{
+   console.log("Can vote.")
+}
+
+//Question 9
+let isWeekend=true;
+//Solution
+if (true) {
+   console.log("No work today!")
+}
+else {
+   console.log("Time to work!")
+}
+//Ans:No work today!
+
+//Question 10
+let num=24;
+//Solution:
+if (num%2===0){
+   console.log("Even Number")
+}
+else{
+   console.log("Odd Number.")
+}
+//Ans:Even Number.
+
+//Exercise 2 done.
